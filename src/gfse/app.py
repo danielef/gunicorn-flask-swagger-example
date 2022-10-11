ad7a7a9ba1e6e426c787e1d0235c255df0f6f511
Daniel Estevez <daniel.ef@gmail.com>
2022-10-10 23:50:38 -0400

    Fix store_true parameters

--- src/gfse/app.py
+++ src/gfse/app.py
@@ -6,7 +6,7 @@ import os
 
 from gfse import api
 
-app = api.create_app(os.getcwd())
+app = api.create_app(os.path.join(os.path.dirname(os.path.realpath(__file__)), "config"))
 
 def load_log(debug):
     log_config = {
@@ -29,16 +29,24 @@ def load_log(debug):
         pass
 
 def main(host, port, debug, use_reloader):
+    print("host: {}".format(host))
+    print("port: {}".format(port))
+    print("debug: {}".format(debug))
     load_log(debug)
     app.run(host=host, port=port, debug=debug, use_reloader=use_reloader)
     
-if __name == '__main__':
+if __name__ == '__main__':
+    default_host               = os.environ.get('API_HOST', '0.0.0.0')
+    default_port               = os.environ.get('API_PORT', 8888)
+    default_debug              = os.environ.get('API_DEBUG', False)
+    default_reloader           = os.environ.get('API_RELOADER', False)
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-b', '--host', default=default_host, help='APIn Host')
     parser.add_argument('-p', '--port', type=int, default=default_port, help='API Port')
-    parser.add_argument('-v', '--verbose', type=bool, default=default_debug, help='API debug')
-    parser.add_argument('-r', '--use-reloader', type=bool, default=default_reloader, help='API use reloader')
+    parser.add_argument('-v', '--verbose', action='store_true', default=default_debug, help='API debug')
+    parser.add_argument('-r', '--use-reloader', action='store_true', default=default_reloader, help='API use reloader')
     args = parser.parse_args()
     
-    main(args.host, args.port, args.debug, args.use_reloader)
+    main(args.host, args.port, args.verbose, args.use_reloader)
     
